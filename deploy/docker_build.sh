0474b7b64787a652c8624c8af4647dddf5108a5a
Chris Bethune <chris.bethune@gmail.com>
2017-05-29 16:44:39 -0400

    moves deploy related scripts to subdir

---
+++ deploy/docker_build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# builds distil-server docker image
+docker build -t docker.uncharted.software/distil-server:0.1 ..
--- docker_build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build -t docker.uncharted.software/distil-server:0.1 .
