35042632015274f89938a3abaa1da61a9f37e2bc
Chris Bethune <cbethune@uncharted.software>
2017-06-12 16:24:48 -0400

    adds test for dataset search route

--- routes/routes_test.go
+++ routes/routes_test.go
@@ -116,3 +116,46 @@ func TestVariablesHandler(t *testing.T) {
 
 	assert.Equal(t, expectedJSON, actualJSON)
 }
+
+func TestDatasetsHandlerWithSearch(t *testing.T) {
+	// load ES result json the test server will return
+	datasetJSON, err := ioutil.ReadFile("./testdata/search.json")
+	assert.NoError(t, err)
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Write(datasetJSON)
+	}
+
+	// put together a stub dataset request
+	request, err := http.NewRequest("GET", "/distil/datasets?search=baseball", nil)
+	assert.NoError(t, err)
+
+	// execute the test request - stubbed ES server will return the JSON
+	// loaded above
+	result, err := testElasticRoute(handler, request, DatasetsHandler)
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, result.Code)
+
+	// compare expected and acutal results - unmarshall first to ensure object rather than byte equality
+	expected := `{
+		"datasets":[
+			{
+				"name": "o_185",
+				"description": "**Author**: Jeffrey S. Simonoff",
+				"variables": [
+					{"name":"d3mIndex","type":"integer"},
+					{"name":"Player","type":"categorical"},
+					{"name":"Number_seasons","type":"integer"},
+					{"name":"Games_played","type":"integer"}
+				]
+			}			
+		]}`
+	expectedJSON, err := gabs.ParseJSON([]byte(expected))
+	assert.NoError(t, err)
+
+	actualJSON, err := gabs.ParseJSON(result.Body.Bytes())
+	assert.NoError(t, err)
+
+	assert.Equal(t, expectedJSON, actualJSON)
+}
