b37bea2f0b7e8e60e3425758a23d56ea22922451
Chris Bethune <cbethune@uncharted.software>
2017-08-04 15:22:09 -0400

    adds create pipeline to websocket request handler

--- api/ws/success.go
+++ api/ws/success.go
@@ -10,11 +10,11 @@ func handleSuccess(conn *Connection, msg *Message, response map[string]interface
 	// append msg id
 	response["id"] = msg.ID
 	// log the response
-	logger := newMessageLogger()
-	logger.messageType(msg.Type)
-	logger.message(msg.Raw)
-	logger.duration(time.Now().Sub(msg.Timestamp))
-	logger.log(true)
+	newMessageLogger().
+		messageType(msg.Type).
+		message(msg.Raw).
+		duration(time.Since(msg.Timestamp)).
+		log(true)
 	// send response
 	err := conn.SendResponse(response)
 	if err != nil {
