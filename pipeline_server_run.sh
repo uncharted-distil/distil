d2012e0e8583823352d195d3b156f7787b7fb6f1
Chris Bethune <cbethune@uncharted.software>
2017-08-31 14:51:15 -0400

    updates test server container to work with dataset volume

--- pipeline_server_run.sh
+++ pipeline_server_run.sh
@@ -2,4 +2,6 @@ docker run \
   --rm \
   --name distil_pipeline_server \
   -p 9500:9500 \
-  docker.uncharted.software/distil-pipeline-server:latest
+  -v `pwd`/datasets:`pwd`/datasets \
+  -e PIPELINE_SERVER_RESULT_DIR=`pwd`/datasets \
+  docker.uncharted.software/distil-pipeline-server
