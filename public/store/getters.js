92b6fdaa5fbef99c57c7d7158de36e1b2a0dddbc
Philippe Horne <phorne@uncharted.software>
2017-09-11 08:11:50 -0400

    Fixed quote used in pipeline getter.

--- public/store/getters.js
+++ public/store/getters.js
@@ -166,7 +166,7 @@ export function getPipelineResults(state) {
 }
 
 export function getWebSocketConnection() {
-	const conn = new Connection(`/ws`, err => {
+	const conn = new Connection('/ws', err => {
 		if (err) {
 			console.warn(err);
 			return;
