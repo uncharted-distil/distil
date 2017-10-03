b7abaeb1f550f13e9abd94ca6f8667b95246f098
Chris Bethune <cbethune@uncharted.software>
2017-10-03 09:07:41 -0400

    makes our ta2 server the default

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PIPELINE_COMPUTE_ENDPOINT=localhost:50051
+export PIPELINE_COMPUTE_ENDPOINT=localhost:9500
 export ES_ENDPOINT=http://localhost:9200
 export PIPELINE_DATA_DIR=`pwd`/datasets
 export PG_STORAGE=true
