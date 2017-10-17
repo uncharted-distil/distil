67fc7325a173c83d4d9555c35a8f5077c8c166db
cbethune <cbethune@uncharted.software>
2017-10-17 14:26:36 -0400

    fixes vue runtime warning

--- pipeline_server_run.sh
+++ pipeline_server_run.sh
@@ -2,7 +2,7 @@ mkdir -p datasets
 docker run \
   --rm \
   --name distil_pipeline_server \
-  -p 9500:9500 \
+  -p 50051:50051 \
   -v `pwd`/datasets:`pwd`/datasets \
   -e PIPELINE_SERVER_RESULT_DIR=`pwd`/datasets \
   docker.uncharted.software/distil-pipeline-server
