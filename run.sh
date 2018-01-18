1bb1ceec452d549a0f357ba00b85a091b3c6748e
Philippe Horne <phorne@uncharted.software>
2018-01-18 16:10:51 -0500

    Went back to common port for TA2 connection.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PIPELINE_COMPUTE_ENDPOINT=localhost:50051
+export PIPELINE_COMPUTE_ENDPOINT=localhost:45042
 export ES_ENDPOINT=http://localhost:9200
 export PIPELINE_DATA_DIR=/datasets
 export PG_STORAGE=true
