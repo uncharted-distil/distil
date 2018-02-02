78da4fb2c9b71b89e79c7ee3b60f1882c92a364b
Philippe Horne <phorne@uncharted.software>
2018-02-02 09:32:38 -0500

    Removed some updates that are handled by a nist eval script.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PIPELINE_COMPUTE_ENDPOINT=localhost:45042
 export ES_ENDPOINT=http://localhost:9200
-export PIPELINE_DATA_DIR=/datasets-isi/data
+export PIPELINE_DATA_DIR=`pwd`/datasets
 export PG_STORAGE=true
 export PIPELINE_COMPUTE_TRACE=true
 export PG_LOG_LEVEL=none # debug, error, warn, info, none
