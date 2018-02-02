67fc2cff2d3116aae793ca55c540c6dea623c4cb
Derrek Pennock <dpennock@uncharted.software>
2018-02-02 08:37:36 -0500

    Added new style to facet type selector

--- public/components/TypeChangeMenu.vue
+++ public/components/TypeChangeMenu.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="type-change-menu">
-		<b-dropdown :text="type" variant="outline-primary" class="var-type-button">
+		<b-dropdown :text="type" variant="secondary" class="var-type-button">
 			<b-dropdown-item
 				v-for="suggested in addMissingSuggestions()"
 				@click.stop="onTypeChange(suggested)"
@@ -55,15 +55,17 @@ export default Vue.extend({
 
 <style>
 
-.var-type-button {
-}
+
 .var-type-button button {
 	border: none;
-	padding: 0;
+	border-radius: 0;
+	padding: 2px 4px;
 	width: 100%;
 	text-align: left;
 	outline: none;
-	font-size: 0.9rem;
+	font-size: 0.867rem;
+	color: white;
+
 }
 .var-type-button button:hover,
 .var-type-button button:active,
@@ -71,9 +73,15 @@ export default Vue.extend({
 .var-type-button.show > .dropdown-toggle  {
 	border: none;
 	border-radius: 0;
-	padding: 0;
-	color: inherit;
-	background-color: inherit;
-	border-color: inherit;
+	padding: 2px 4px;
+	color: white;
+	background-color: #424242;
+	border-color: #424242;
+	box-shadow: none;
 }
+.type-change-menu .dropdown-item {
+	font-size: 0.867rem;
+	text-transform: none;
+}
+
 </style>
--- public/components/VariableFacets.vue
+++ public/components/VariableFacets.vue
@@ -377,6 +377,14 @@ button {
     box-shadow: 0 1px 2px 0 rgba(0,0,0,0.10);
     transition: box-shadow 0.3s ease-in-out;
 }
+.variable-facets-container .facets-root-container .facets-group-container .facets-group .group-header {
+	padding: 4px 0px 6px 8px;
+
+}
+.variable-facets-container .facets-root-container .facets-group-container .facets-group .group-header .type-change-menu {
+	float: right;
+	margin-top: -4px;
+}
 .facet-filters span {
 	font-size: 0.9rem;
 }
