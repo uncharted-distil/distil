75f8766cf6ea2e318d7011e6659eca10079e2137
Philippe Horne <phorne@uncharted.software>
2018-02-14 11:30:07 -0500

    Unknown type added as classification problem.

--- public/util/pipelines.ts
+++ public/util/pipelines.ts
@@ -204,5 +204,6 @@ const variableType: Dictionary<Task> = {
 	postal_code: classification,
 	uri: classification,
 	datetime: classification,
-	text: classification
+	text: classification,
+	unknown: classification
 };
