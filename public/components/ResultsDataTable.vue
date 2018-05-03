b8ffbeac4859d148cce11e7c132e923c9e3c90b6
kbirk <birk.kevin@gmail.com>
2018-05-03 16:59:58 -0400

    Add error bar to error column in result table.

--- public/components/ResultsDataTable.vue
+++ public/components/ResultsDataTable.vue
@@ -18,6 +18,15 @@
 				:items="items"
 				:fields="fields"
 				@row-clicked="onRowClick">
+				<template :slot="targetErrorCol" slot-scope="data">
+					<!-- A custom formatted data column cell -->
+
+					<div class="error-bar-container">
+						<div class="error-bar" v-bind:style="{ 'background-color': errorBarColor(data.item[targetErrorCol]), width: errorBarWidth(data.item[targetErrorCol]), left: errorBarLeft(data.item[targetErrorCol]) }"></div>
+						<div class="error-bar-center"></div>
+					</div>
+
+				</template>
 			</b-table>
 		</div>
 
@@ -29,11 +38,13 @@
 import _ from 'lodash';
 import { spinnerHTML } from '../util/spinner';
 import { getters } from '../store/data/module';
+import { Extrema } from '../store/data/index';
 import { TargetRow, TableRow, FieldInfo, RowSelection } from '../store/data/index';
+import { getters as dataGetters } from '../store/data/module';
 import { getters as routeGetters } from '../store/route/module';
 import { getters as pipelineGetters } from '../store/pipelines/module';
 import { Dictionary } from '../util/dict';
-import { removeNonTrainingItems, removeNonTrainingFields } from '../util/data';
+import { removeNonTrainingItems, removeNonTrainingFields, getErrorCol } from '../util/data';
 import { updateRowSelection, clearRowSelection, updateTableRowSelection } from '../util/row';
 import Vue from 'vue';
 
@@ -55,6 +66,18 @@ export default Vue.extend({
 
 		numRows(): number {
 			return getters.getResultDataNumRows(this.$store);
+		},
+
+		target(): string {
+			return routeGetters.getRouteTargetVariable(this.$store);
+		},
+
+		targetErrorCol(): string {
+			return getErrorCol(this.target);
+		},
+
+		residualExtrema(): Extrema {
+			return dataGetters.getResidualExtrema(this.$store);
 		},
 
 		training(): Dictionary<boolean> {
@@ -91,7 +114,15 @@ export default Vue.extend({
 
 		spinnerHTML(): string {
 			return spinnerHTML();
-		}
+		},
+
+		residualThresholdMin(): number {
+			return _.toNumber(routeGetters.getRouteResidualThresholdMin(this.$store));
+		},
+
+		residualThresholdMax(): number {
+			return _.toNumber(routeGetters.getRouteResidualThresholdMax(this.$store));
+		},
 	},
 
 	methods: {
@@ -114,6 +145,31 @@ export default Vue.extend({
 				clearRowSelection(this);
 			}
 		},
+
+		normalizeError(error: number): number {
+			const range = this.residualExtrema.max - this.residualExtrema.min;
+			return (error - this.residualExtrema.min) / range;
+		},
+
+		errorBarWidth(error: number): string {
+			return `${Math.abs((this.normalizeError(error)*50))}%`;
+		},
+
+		errorBarLeft(error: number): string {
+			const nerr = this.normalizeError(error);
+			if (nerr > 0) {
+				return '50%';
+			}
+			return `${50 + nerr * 50}%`;
+		},
+
+		errorBarColor(error: number): string {
+			if (error < this.residualThresholdMin || error > this.residualThresholdMax) {
+				return '#e05353';
+			}
+			return '#9e9e9e';
+		}
+
 	}
 
 });
@@ -137,6 +193,27 @@ export default Vue.extend({
 }
 table tr {
 	cursor: pointer;
+}
+
+.error-bar-container {
+	position: relative;
+	width: 80px;
+	height: 18px;
+}
+
+.error-bar {
+	position: absolute;
+	height: 80%;
+	bottom: 0;
+}
+
+.error-bar-center {
+	position: absolute;
+	width: 1px;
+	height: 90%;
+	left: 50%;
+	bottom: 0;
+	background-color: #666;
 }
 
 </style>
