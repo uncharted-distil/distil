753f1e6d47385ebbf48af2806cbb9099d3959010
cbethune <cbethune@uncharted.software>
2018-07-14 09:28:19 -0400

    typing fixes from nav.ts

--- public/util/nav.ts
+++ public/util/nav.ts
@@ -1,33 +1,33 @@
 import VueRouter from 'vue-router';
-import { Store } from 'vuex';
+import store from '../store/store';
 import { createRouteEntry } from '../util/routes';
 import { restoreView } from '../util/view';
 import { HOME_ROUTE, SEARCH_ROUTE, SELECT_ROUTE, CREATE_ROUTE, RESULTS_ROUTE } from '../store/route/index';
 import { getters as routeGetters } from '../store/route/module';
 
-export function gotoView(store: Store<any>, router: VueRouter, view: string) {
+export function gotoView(router: VueRouter, view: string) {
 	const dataset = routeGetters.getRouteDataset(store);
 	const prev = restoreView(store, view, dataset);
 	const entry = createRouteEntry(view, prev ? prev.query : {});
 	router.push(entry);
 }
 
-export function gotoHome(store: Store<any>, router: VueRouter) {
-	gotoView(store, router, HOME_ROUTE);
+export function gotoHome(router: VueRouter) {
+	gotoView(router, HOME_ROUTE);
 }
 
-export function gotoSearch(store: Store<any>, router: VueRouter) {
-	gotoView(store, router, SEARCH_ROUTE);
+export function gotoSearch(router: VueRouter) {
+	gotoView(router, SEARCH_ROUTE);
 }
 
-export function gotoSelectTarget(store: Store<any>, router: VueRouter) {
-	gotoView(store, router, SELECT_ROUTE);
+export function gotoSelectTarget(router: VueRouter) {
+	gotoView(router, SELECT_ROUTE);
 }
 
-export function gotoSelectData(store: Store<any>, router: VueRouter) {
-	gotoView(store, router, CREATE_ROUTE);
+export function gotoSelectData(router: VueRouter) {
+	gotoView(router, CREATE_ROUTE);
 }
 
-export function gotoResults(store: Store<any>, router: VueRouter) {
-	gotoView(store, router, RESULTS_ROUTE);
+export function gotoResults(router: VueRouter) {
+	gotoView(router, RESULTS_ROUTE);
 }
