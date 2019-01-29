51ee1d89a2e534875cfab10467c4e5e9a5f7451e
Chris Bethune <cbethune@uncharted.software>
2019-01-29 10:42:36 -0500

    fixes dataset fetch on run + adds logging to datamart

--- api/middleware/log_rest_client.go
+++ api/middleware/log_rest_client.go
@@ -1,9 +1,10 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
-	"time"
+	"net/http/httputil"
+
+	log "github.com/unchartedsoftware/plog"
 )
 
 // LoggingClient is an http.Client that logs *outoing* REST requests.
@@ -13,21 +14,28 @@ type LoggingClient struct {
 
 // Do wraps the basic http.Client.Do call to log requests and responses.
 func (c *LoggingClient) Do(req *http.Request) (*http.Response, error) {
+
+	reqDump, _ := httputil.DumpRequestOut(req, true)
+	log.Infof("%s", reqDump)
+
 	// execute the request
-	t1 := time.Now()
+	// t1 := time.Now()
 	resp, err := c.Client.Do(req)
 	if err != nil {
 		return nil, err
 	}
-	t2 := time.Now()
+	// t2 := time.Now()
 
-	newRequestLogger().
-		requestType(fmt.Sprintf("REST CLIENT %s", req.Method)).
-		request(req.URL.String()).
-		params(req.URL.String()).
-		status(resp.StatusCode).
-		duration(t2.Sub(t1)).
-		log(resp.StatusCode < 500)
+	respDump, _ := httputil.DumpResponse(resp, false)
+	log.Infof("%s", respDump)
+
+	// newRequestLogger().
+	// 	requestType(fmt.Sprintf("REST CLIENT %s", req.Method)).
+	// 	request(req.URL.String()).
+	// 	params(req.URL.String()).
+	// 	status(resp.StatusCode).
+	// 	duration(t2.Sub(t1)).
+	// log(resp.StatusCode < 500)
 
 	return resp, err
 }
