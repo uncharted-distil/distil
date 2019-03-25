0aee26a0dc3f863dbad0b8a1563af55845c7a05d
Jaehwan Ryu <jryu@uncharted.software>
2019-03-25 19:11:36 -0400

    Added components for notification panel UI

---
+++ public/components/NotificationPanel.vue
@@ -0,0 +1,34 @@
+<template>
+
+<div class="notification-panel">
+
+</div>
+    
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: 'notification-panel',
+});
+
+</script>
+
+<style>
+
+.notification-panel {
+	box-shadow: 0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px -1px rgba(0,0,0,0.2);
+	display: block;
+	position: fixed;
+	right: 0;
+	top: 0;
+	bottom: 0;
+	z-index: 100;
+	width: 300px;
+	height: 100%;
+	background: #fff;
+}
+
+</style>
---
+++ public/components/NotificationSidebar.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="notification-sidebar">
+    <div class="notification-icons">
+        <div class="event-icon">
+            <i class="fa fa-2x fa-info" aria-hidden="true"></i>
+        </div>
+        <div class="event-icon">
+            <i class="fa fa-2x fa-plus-square" aria-hidden="true"></i>
+        </div>
+        <div class="event-icon">
+            <i class="fa fa-2x fa-long-arrow-right" aria-hidden="true"></i>
+        </div>
+    </div>
+    <div class="notification-detail">
+
+    </div>
+</div>
+    
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: 'notification-sidebar',
+});
+
+</script>
+
+<style>
+
+.notification-sidebar {
+	background-color: #fff;
+	width: 55px;
+	border-left: 1px solid #ccc;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+}
+.notification-sidebar .event-icon {
+	padding-top: 15px;
+	padding-bottom: 15px;
+	text-align: center;
+}
+.event-icon i {
+	height: 30px;
+	width: 30px;
+	cursor: pointer;
+}
+
+</style>
