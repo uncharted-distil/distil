58b5aba0eb972948fb2d9a05d70de8151f87af13
cbethune <cbethune@uncharted.software>
2019-06-25 00:05:20 -0400

    extending pull timeout for better distil-automl-interop

--- run.sh
+++ run.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 export SOLUTION_COMPUTE_ENDPOINT=localhost:45042
 export ES_ENDPOINT=http://localhost:9200
-export PG_STORAGE=true
 export SOLUTION_COMPUTE_TRACE=true
 export PG_LOG_LEVEL=none # debug, error, warn, info, none
 export SKIP_INGEST=true
 export ROOT_RESOURCE_DIRECTORY=http://localhost:5440
 export D3MINPUTDIR_ROOT=`pwd`/datasets
 export D3MINPUTDIR=`pwd`/datasets
-export D3MOUTPUTDIR=`pwd`/outputs
-export TEMP_STORAGE_ROOT=`pwd`/outputs/temp
 export USER_PROBLEM_PATH=`pwd`/outputs/problems
 export SOLUTION_SEARCH_MAX_TIME=3
+export SOLUTION_COMPUTE_PULL_MAX=900
 export USE_TA2_RUNNER=true #false
 export DATAMART_IMPORT_FOLDER=`pwd`/datamart
 
