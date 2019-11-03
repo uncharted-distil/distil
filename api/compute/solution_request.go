774762dd9e9460daa911f4188c84933aed0c51a1
Chris Bethune <cbethune@uncharted.software>
2019-11-02 22:10:40 -0500

    corrects variable used for task determination

--- api/compute/solution_request.go
+++ api/compute/solution_request.go
@@ -665,7 +665,7 @@ func (s *SolutionRequest) PersistAndDispatch(client *compute.Client, solutionSto
 	datasetInputDir := env.ResolvePath(datasetInput.Source, datasetInput.Folder)
 
 	// compute the task and subtask from the target and dataset
-	task, err := ResolveTask(dataStorage, dataset.Metadata.StorageName, targetVariable)
+	task, err := ResolveTask(dataStorage, dataset.Metadata.StorageName, groupingTargetVariable)
 	if err != nil {
 		return err
 	}
