664cfc8f3092f27828f8f523a3b6c4b35edcec6d
philippe horne <phorne@uncharted.software>
2019-11-08 16:21:50 -0500

    Added param logging for discovery logger.

--- api/env/log.go
+++ api/env/log.go
@@ -2,6 +2,7 @@ package env
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"os"
 	"path"
@@ -99,31 +100,32 @@ func LogDatamartActionGlobal(feature string, activity string, subActivity string
 
 // LogSystemAction logs a system action to the discovery log.
 func (l *DiscoveryLogger) LogSystemAction(feature string, activity string, subActivity string) {
-	l.logAction(feature, "SYSTEM", activity, subActivity)
+	l.logAction(feature, "SYSTEM", activity, subActivity, nil)
 }
 
 // LogAPIAction logs a TA2TA3 API call to the discovery log.
-func (l *DiscoveryLogger) LogAPIAction(method string) {
+func (l *DiscoveryLogger) LogAPIAction(method string, params map[string]string) {
 	// look up the feature, the activity and sub activity based on the grpc method
 	feature := featureMap[method]
 	if feature == "" {
 		feature = method
 	}
-	l.logAction(feature, "TA2TA3", activityMap[method], subActivityMap[method])
+	l.logAction(feature, "TA2TA3", activityMap[method], subActivityMap[method], params)
 }
 
 // LogDatamartAction logs a datamart fuction call to the discovery log.
 func (l *DiscoveryLogger) LogDatamartAction(feature string, activity string, subActivity string) {
-	l.logAction(feature, "DATAMART", activity, subActivity)
+	l.logAction(feature, "DATAMART", activity, subActivity, nil)
 }
 
-func (l *DiscoveryLogger) logAction(feature string, typ string, activity string, subActivity string) {
+func (l *DiscoveryLogger) logAction(feature string, typ string, activity string, subActivity string, params map[string]string) {
 	timestamp := fmt.Sprintf(time.Now().Format(time.RFC3339))
+	paramsString, _ := json.Marshal(params)
 
 	mu.Lock()
 	defer mu.Unlock()
 	f, _ := os.OpenFile(l.csvFilename, os.O_WRONLY|os.O_APPEND, os.ModePerm)
 	w := csv.NewWriter(f)
-	w.Write([]string{timestamp, feature, typ, activity, subActivity, "{}"})
+	w.Write([]string{timestamp, feature, typ, activity, subActivity, string(paramsString)})
 	w.Flush()
 }
