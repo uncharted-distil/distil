ad469cccc38645108fc78bc75d2642cb50dd1f70
philippe horne <phorne@uncharted.software>
2019-12-01 15:34:04 -0500

    Working on produce task to create predictions from a new dataset using an existing model.

--- api/task/prediction.go
+++ api/task/prediction.go
@@ -15,59 +15,12 @@
 
 package task
 
-import (
-	"os"
-	"path"
-
-	"github.com/uncharted-distil/distil-compute/model"
-	"github.com/uncharted-distil/distil-compute/primitive/compute"
-
-	"github.com/uncharted-distil/distil-ingest/metadata"
-	"github.com/uncharted-distil/distil/api/util"
-)
-
 // Predict processes input data to generate predictions.
-func Predict(dataset string, fittedSolutionID string, csvData []byte) (string, error) {
-	// save the csv file in the file system datasets folder
-	outputDatasetPath := path.Join(outputPath, dataset)
-	dataFilePath := path.Join(compute.D3MDataFolder, compute.D3MLearningData)
-	dataPath := path.Join(outputDatasetPath, dataFilePath)
-	err := util.WriteFileWithDirs(dataPath, csvData, os.ModePerm)
+func Predict(dataset string, fittedSolutionID string, csvData []byte, outputPath string, config *IngestTaskConfig) (string, error) {
+	// create the dataset to be used for predictions
+	path, err := CreateDataset(dataset, csvData, outputPath, config)
 	if err != nil {
-		return "", err
+		return "", nil
 	}
 
-	// create the raw dataset schema doc
-	datasetID := model.NormalizeDatasetID(dataset)
-	meta := model.NewMetadata(dataset, dataset, "", datasetID)
-	dr := model.NewDataResource("learningData", model.ResTypeRaw, []string{compute.D3MResourceFormat})
-	dr.ResPath = dataFilePath
-	meta.DataResources = []*model.DataResource{dr}
-
-	schemaPath := path.Join(outputDatasetPath, compute.D3MDataSchema)
-	err = metadata.WriteSchema(meta, schemaPath)
-	if err != nil {
-		return "", err
-	}
-
-	// format the dataset into a D3M format
-	formattedPath, err := Format(metadata.Contrib, schemaPath, config)
-	if err != nil {
-		return "", err
-	}
-
-	// copy to the original output location for consistency
-	if formattedPath != outputDatasetPath {
-		err = os.RemoveAll(outputDatasetPath)
-		if err != nil {
-			return "", err
-		}
-
-		err = util.Copy(formattedPath, path.Dir(schemaPath))
-		if err != nil {
-			return "", err
-		}
-	}
-
-	return formattedPath, nil
 }
