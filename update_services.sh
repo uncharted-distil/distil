9acf3e1436d64ac5a4d1500f51daf56fae864d91
philippe horne <phorne@uncharted.software>
2020-04-26 09:10:37 -0400

    Set datamart import folder in docker compose, as well as supporting shell scripts.

--- update_services.sh
+++ update_services.sh
@@ -12,5 +12,8 @@ if [[ -z "${D3MSTATICDIR}" ]]; then
     export D3MSTATICDIR="$PWD/static_resources"
 fi
 
- docker-compose pull 
-  
+if [[ -z "${DATAMART_IMPORT_FOLDER}" ]]; then
+  export DATAMART_IMPORT_FOLDER="$PWD/datamart"
+fi
+
+ docker-compose pull
