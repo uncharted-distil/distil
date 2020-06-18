9b301365c2e7e89d38384dbbc5574d62f2a8ee64
Chris Bethune <cbethune@uncharted.software>
2020-06-18 16:12:36 -0400

    removes debug file

--- pg.session.sql
+++
@@ -1,7 +0,0 @@
--- SELECT MIN("coordinates" [1]) AS "min_x",
---     MAX("coordinates" [5]) AS "max_x",
---     MIN("coordinates" [2]) AS "min_y",
---     MAX("coordinates" [6]) AS "max_y"
--- FROM d_bigearth_tiny;
-SELECT "coordinates[1]"
-FROM d_bigearth_tiny;
