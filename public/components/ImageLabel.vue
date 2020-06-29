c46bad63ec559e3ff801548f901edc767bdcbb83
Chris Bethune <cbethune@uncharted.software>
2020-06-29 18:31:30 -0400

    switches to use separated label component

---
+++ public/components/ImageLabel.vue
@@ -0,0 +1,188 @@
+<template>
+  <ol class="labels">
+    <li
+      v-for="(label, index) in labels"
+      :key="index"
+      class="label"
+      :class="label.status"
+    >
+      {{ label.value }}
+    </li>
+  </ol>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Dictionary } from "../util/dict";
+import { TableRow, TableColumn, VariableSummary } from "../store/dataset/index";
+import { getters as datasetGetters } from "../store/dataset/module";
+import { getters as requestGetters } from "../store/requests/module";
+import { getters as routeGetters } from "../store/route/module";
+import { getters as resultGetters } from "../store/results/module";
+import _ from "lodash";
+
+interface Label {
+  status: string;
+  value: string;
+}
+
+/**
+ * Display the prediction result as a label.
+ */
+export default Vue.extend({
+  name: "image-label",
+
+  components: {},
+
+  data() {
+    return {};
+  },
+
+  props: {
+    dataFields: Object as () => Dictionary<TableColumn>,
+    includedActive: Boolean as () => boolean,
+    item: Object as () => TableRow,
+    shortenLabels: Boolean as () => boolean
+  },
+
+  computed: {
+    fields(): Dictionary<TableColumn> {
+      return this.dataFields
+        ? this.dataFields
+        : this.includedActive
+        ? datasetGetters.getIncludedTableDataFields(this.$store)
+        : datasetGetters.getExcludedTableDataFields(this.$store);
+    },
+
+    labels(): Label[] {
+      const labels = [];
+      let status;
+
+      for (const key in this.fields) {
+        status = null;
+
+        // Define status of label (correct or incorrect) if we want to show the predicted error.
+        if (key === this.predictedField && this.showError) {
+          status = this.correct() ? "correct" : "incorrect";
+        }
+
+        // Display the label
+        if (key === this.targetField || key === this.predictedField) {
+          const label = this.shortenLabels
+            ? this.shortenLabel(this.item[key].value)
+            : this.item[key].value;
+          labels.push({ status, value: label } as Label);
+        }
+      }
+
+      return labels;
+    },
+
+    predictedField(): string {
+      const predictions = requestGetters.getActivePredictions(this.$store);
+      if (predictions) {
+        return predictions.predictedKey;
+      }
+
+      const solution = requestGetters.getActiveSolution(this.$store);
+      return solution ? `${solution.predictedKey}` : "";
+    },
+
+    showError(): boolean {
+      return (
+        this.predictedField && !requestGetters.getActivePredictions(this.$store)
+      );
+    },
+
+    targetField(): string {
+      return routeGetters.getRouteTargetVariable(this.$store);
+    },
+
+    // Creates a dictionary of label lengths keyed by a label's first character.  This supports
+    // the generation of a minimum ambiguous label by starting letter.  For the set [Airport, Agriculture, Forest],
+    // we will end up with [A=4, F=1], which at runtime will generate display lables of [Ai, Ag, F].  This is a computed
+    // property so it should only end up being updated when the underlying data changes.
+    labelLengths(): Dictionary<number> {
+      let summary: VariableSummary = null;
+      // find the target variable and get the prediction labels
+      if (this.showError) {
+        summary = resultGetters.getTargetSummary(this.$store);
+      } else {
+        summary = datasetGetters
+          .getVariableSummaries(this.$store)
+          .find(v => v.key === this.targetField);
+      }
+      const bucketNames = summary.baseline.buckets.map(b => b.key);
+      // If this isn't categorical, don't generate the table.
+      if (!bucketNames) {
+        return {};
+      }
+
+      // initailize label lengths with zeroes
+      const imageLabelLengths: Dictionary<number> = {};
+      bucketNames.forEach(b => (imageLabelLengths[b[0]] = 0));
+
+      // Compare each label to the others
+      for (let i = 0; i < bucketNames.length; i++) {
+        const currLabel = bucketNames[i];
+        for (let j = i + 1; j < bucketNames.length; j++) {
+          const compareLabel = bucketNames[j];
+          // Update the min number of characters required to disambiguate the labels
+          // with the same starting character.
+          if (currLabel[0] === compareLabel[0]) {
+            for (
+              let k = imageLabelLengths[currLabel[0]];
+              k < Math.min(currLabel.length, compareLabel.length);
+              k++
+            ) {
+              if (currLabel[k] !== compareLabel[k]) {
+                break;
+              }
+              imageLabelLengths[currLabel[0]] += 1;
+            }
+          }
+        }
+      }
+      return imageLabelLengths;
+    }
+  },
+
+  methods: {
+    correct(): boolean {
+      return (
+        this.item[this.targetField].value ===
+        this.item[this.predictedField].value
+      );
+    },
+
+    // Given a raw label value, returns shortened label that is unique amongst the set of target labels.
+    shortenLabel(rawLabel: string): string {
+      const labelLength = this.labelLengths[rawLabel[0]];
+      return _.isNil(labelLength)
+        ? rawLabel
+        : rawLabel.substring(0, labelLength + 1);
+    }
+  }
+});
+</script>
+
+<style scoped>
+.labels {
+  color: white;
+  list-style: none;
+  list-style-position: outside;
+  max-width: 100%;
+}
+
+.label {
+  background-color: #424242;
+  overflow: hidden;
+  padding: 0 2px;
+}
+.label.correct {
+  background-color: #03c003;
+}
+.label.incorrect {
+  background-color: #be0000;
+}
+</style>
--- public/components/ImageMosaic.vue
+++ public/components/ImageMosaic.vue
@@ -13,40 +13,16 @@
               :height="imageHeight"
               :on-click="onImageClick"
               :type="imageField.type"
+              :key="fieldKey"
             ></image-preview>
           </template>
-          <div v-if="showError" class="image-label-container">
-            <template v-for="(fieldInfo, fieldKey) in fields">
-              <div
-                v-if="fieldKey == predictedField && correct(item)"
-                class="image-label-correct"
-              >
-                {{ shortenLabel(item[fieldKey].value) }}
-              </div>
-              <div
-                v-if="fieldKey == targetField && !correct(item)"
-                class="image-label"
-              >
-                {{ shortenLabel(item[fieldKey].value) }}
-              </div>
-              <div
-                v-if="fieldKey == predictedField && !correct(item)"
-                class="image-label-incorrect"
-              >
-                {{ shortenLabel(item[fieldKey].value) }}
-              </div>
-            </template>
-          </div>
-          <div v-if="!showError" class="image-label-container">
-            <template v-for="(fieldInfo, fieldKey) in fields">
-              <div
-                v-if="fieldKey == targetField || fieldKey == predictedField"
-                class="image-label"
-              >
-                {{ shortenLabel(item[fieldKey].value) }}
-              </div>
-            </template>
-          </div>
+          <image-label
+            class="image-label"
+            :dataFields="dataFields"
+            includedActive
+            shortenLabels
+            :item="item"
+          />
         </div>
       </template>
     </template>
@@ -54,11 +30,9 @@
 </template>
 
 <script lang="ts">
-import _ from "lodash";
 import Vue from "vue";
+import ImageLabel from "./ImageLabel";
 import ImagePreview from "./ImagePreview";
-import { getters as datasetGetters } from "../store/dataset/module";
-import { getters as requestGetters } from "../store/requests/module";
 import {
   RowSelection,
   TableColumn,
@@ -68,8 +42,10 @@ import {
   Variable,
   VariableSummary
 } from "../store/dataset/index";
+import { getters as datasetGetters } from "../store/dataset/module";
 import { getters as routeGetters } from "../store/route/module";
 import { getters as resultGetters } from "../store/results/module";
+import { getters as requestGetters } from "../store/requests/module";
 import { Dictionary } from "../util/dict";
 import {
   addRowSelection,
@@ -86,6 +62,7 @@ export default Vue.extend({
   name: "image-mosaic",
 
   components: {
+    ImageLabel,
     ImagePreview
   },
 
@@ -153,54 +130,6 @@ export default Vue.extend({
       return (
         this.predictedField && !requestGetters.getActivePredictions(this.$store)
       );
-    },
-
-    // Creates a dictionary of label lengths keyed by a label's first character.  This supports
-    // the generation of a minimum ambiguous label by starting letter.  For the set [Airport, Agriculture, Forest],
-    // we will end up with [A=4, F=1], which at runtime will generate display lables of [Ai, Ag, F].  This is a computed
-    // property so it should only end up being updated when the underlying data changes.
-    labelLengths(): Dictionary<number> {
-      let summary: VariableSummary = null;
-      // find the target variable and get the prediction labels
-      if (this.showError) {
-        summary = resultGetters.getTargetSummary(this.$store);
-      } else {
-        summary = datasetGetters
-          .getVariableSummaries(this.$store)
-          .find(v => v.key === this.targetField);
-      }
-      const bucketNames = summary.baseline.buckets.map(b => b.key);
-      // If this isn't categorical, don't generate the table.
-      if (!bucketNames) {
-        return {};
-      }
-
-      // initailize label lengths with zeroes
-      const imageLabelLengths: Dictionary<number> = {};
-      bucketNames.forEach(b => (imageLabelLengths[b[0]] = 0));
-
-      // Compare each label to the others
-      for (let i = 0; i < bucketNames.length; i++) {
-        const currLabel = bucketNames[i];
-        for (let j = i + 1; j < bucketNames.length; j++) {
-          const compareLabel = bucketNames[j];
-          // Update the min number of characters required to disambiguate the labels
-          // with the same starting character.
-          if (currLabel[0] === compareLabel[0]) {
-            for (
-              let k = imageLabelLengths[currLabel[0]];
-              k < Math.min(currLabel.length, compareLabel.length);
-              k++
-            ) {
-              if (currLabel[k] !== compareLabel[k]) {
-                break;
-              }
-              imageLabelLengths[currLabel[0]] += 1;
-            }
-          }
-        }
-      }
-      return imageLabelLengths;
     }
   },
 
@@ -221,18 +150,6 @@ export default Vue.extend({
           event.row[D3M_INDEX_FIELD]
         );
       }
-    },
-
-    correct(item: any): boolean {
-      return item[this.targetField].value === item[this.predictedField].value;
-    },
-
-    // Given a raw label value, returns shortened label that is unique amongst the set of target labels.
-    shortenLabel(rawLabel: string): string {
-      const labelLength = this.labelLengths[rawLabel[0]];
-      return _.isNil(labelLength)
-        ? rawLabel
-        : rawLabel.substring(0, labelLength + 1);
     }
   }
 });
@@ -258,34 +175,11 @@ export default Vue.extend({
   position: relative;
 }
 
-.image-label-container {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  z-index: 1;
-}
-
 .image-label {
-  float: right;
-  background-color: #424242;
-  color: #fff;
-  padding: 0 4px;
-  margin: 0, 2px;
-}
-
-.image-label-correct {
-  float: right;
-  background-color: #03c003;
-  color: #fff;
-  padding: 0 4px;
-  margin: 0, 2px;
-}
-
-.image-label-incorrect {
-  float: right;
-  background-color: #be0000;
-  color: #fff;
-  padding: 0 4px;
-  margin: 0, 2px;
+  position: absolute;
+  left: 2px;
+  top: 2px;
+  padding: 0 2px;
+  z-index: 1;
 }
 </style>
