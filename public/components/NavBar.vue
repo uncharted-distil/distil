fcdef8684bd7c690d5a0180d5d3c4f98ab4f997b
Yohann Paris <yparis@uncharted.software>
2020-07-17 14:48:56 -0400

    Redesign the navbar using uncharted bootstrap them variables and new design colours. Commented the CSS to be more readable. Creating one workflow.

--- public/components/NavBar.vue
+++ public/components/NavBar.vue
@@ -1,88 +1,99 @@
 <template>
-  <b-navbar
-    toggleable="md"
-    type="dark"
-    class="navbar-fixed-top bottom-shadowed"
-  >
+  <b-navbar toggleable="md" type="dark" class="fixed-top">
     <b-nav-toggle target="nav-collapse"></b-nav-toggle>
 
-    <img src="/images/uncharted.svg" class="app-icon" />
-    <span class="navbar-brand">Distil</span>
-    <b-navbar-nav>
-      <b-nav-item
-        @click="onHome"
-        :active="isActive(HOME_ROUTE)"
-        v-bind:class="{ active: isActive(HOME_ROUTE) }"
-      >
-        <i class="fa fa-home nav-icon"></i>
-        <b-nav-text>Home</b-nav-text>
-      </b-nav-item>
-      <b-nav-item
-        @click="onSearch"
-        :active="isActive(SEARCH_ROUTE)"
-        v-bind:class="{ active: isActive(SEARCH_ROUTE) }"
-      >
-        <i class="fa fa-angle-right nav-arrow"></i>
-        <i class="fa fa-file-text-o nav-icon"></i>
-        <b-nav-text>Select Data</b-nav-text>
-      </b-nav-item>
-      <b-nav-item
-        @click="onSelectTarget"
-        :active="isActive(SELECT_TARGET_ROUTE)"
-        :disabled="!hasView(SELECT_TARGET_ROUTE)"
-        v-bind:class="{ active: isActive(SELECT_TARGET_ROUTE) }"
-      >
-        <i class="fa fa-angle-right nav-arrow"></i>
-        <i class="fa fa-dot-circle-o  nav-icon"></i>
-        <b-nav-text>Select Target</b-nav-text>
-      </b-nav-item>
-      <b-nav-item
-        @click="onSelectData"
-        :active="isActive(SELECT_TRAINING_ROUTE)"
-        :disabled="!hasView(SELECT_TRAINING_ROUTE)"
-        v-bind:class="{ active: isActive(SELECT_TRAINING_ROUTE) }"
-      >
-        <i class="fa fa-angle-right nav-arrow"></i>
-        <i class="fa fa-code-fork  nav-icon"></i>
-        <b-nav-text>Create Models</b-nav-text>
-      </b-nav-item>
-      <b-nav-item
-        @click="onJoinDatasets"
-        v-if="isJoinDatasets && isActive(JOIN_DATASETS_ROUTE)"
-        :active="isActive(JOIN_DATASETS_ROUTE)"
-        v-bind:class="{ active: isActive(JOIN_DATASETS_ROUTE) }"
-      >
-        <i class="fa fa-angle-right nav-arrow"></i>
-        <i class="fa fa-database nav-icon"></i>
-        <b-nav-text>Join Datasets</b-nav-text>
-      </b-nav-item>
-      <b-nav-item
-        @click="onResults"
-        :active="isActive(RESULTS_ROUTE)"
-        :disabled="!hasView(RESULTS_ROUTE)"
-        v-bind:class="{ active: isActive(RESULTS_ROUTE) }"
-      >
-        <i class="fa fa-angle-right nav-arrow"></i>
-        <i class="fa fa-line-chart nav-icon"></i>
-        <b-nav-text>View Models</b-nav-text>
-      </b-nav-item>
-      <b-nav-item
-        @click="onPredictions"
-        :active="isActive(PREDICTION_ROUTE)"
-        :disabled="!hasView(PREDICTION_ROUTE)"
-        v-bind:class="{ active: isActive(PREDICTION_ROUTE) }"
-      >
-        <i class="fa fa-angle-right nav-arrow"></i>
-        <i class="fa fa-eye nav-icon"></i>
-        <b-nav-text>View Predictions</b-nav-text>
-      </b-nav-item>
-    </b-navbar-nav>
+    <!-- Branding -->
+    <img src="/images/uncharted.svg" class="app-icon" height="36" width="36" />
+    <b-navbar-brand>Distil</b-navbar-brand>
+
+    <!-- Left Side -->
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item @click="onSearch" :active="isActive(SEARCH_ROUTE)">
+          <i class="fa fa-home nav-icon"></i> Select Model or Dataset
+        </b-nav-item>
+
+        <b-nav-item
+          @click="onSelectTarget"
+          :active="isActive(SELECT_TARGET_ROUTE)"
+          :disabled="!hasView(SELECT_TARGET_ROUTE)"
+        >
+          <i class="fa fa-dot-circle-o nav-icon"></i> New Model: Select Target
+        </b-nav-item>
+
+        <b-nav-item
+          @click="onSelectData"
+          :active="isActive(SELECT_TRAINING_ROUTE)"
+          :disabled="!hasView(SELECT_TRAINING_ROUTE)"
+        >
+          <i class="fa fa-sign-in nav-icon"></i> Select Model Features
+        </b-nav-item>
+
+        <b-nav-item
+          @click="onResults"
+          :active="isActive(RESULTS_ROUTE)"
+          :disabled="!hasView(RESULTS_ROUTE)"
+        >
+          <i class="fa fa-check-circle nav-icon"></i> Check Models
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+
+    <!--
+        <b-nav-item
+          @click="onSearch"
+          :active="isActive(SEARCH_ROUTE)"
+        >
+          <i class="fa fa-file-text-o nav-icon"></i>
+          <b-nav-text>Select Data</b-nav-text>
+        </b-nav-item>
+        <b-nav-item
+          @click="onSelectTarget"
+          :active="isActive(SELECT_TARGET_ROUTE)"
+          :disabled="!hasView(SELECT_TARGET_ROUTE)"
+        >
+          <i class="fa fa-dot-circle-o nav-icon"></i>
+          <b-nav-text>Select Target</b-nav-text>
+        </b-nav-item>
+        <b-nav-item
+          @click="onSelectData"
+          :active="isActive(SELECT_TRAINING_ROUTE)"
+          :disabled="!hasView(SELECT_TRAINING_ROUTE)"
+        >
+          <i class="fa fa-code-fork nav-icon"></i>
+          <b-nav-text>Create Models</b-nav-text>
+        </b-nav-item>
+        <b-nav-item
+          @click="onJoinDatasets"
+          v-if="isJoinDatasets && isActive(JOIN_DATASETS_ROUTE)"
+          :active="isActive(JOIN_DATASETS_ROUTE)"
+          v-bind:class="{ active: isActive(JOIN_DATASETS_ROUTE) }"
+        >
+          <i class="fa fa-angle-right nav-arrow"></i>
+          <i class="fa fa-database nav-icon"></i>
+          <b-nav-text>Join Datasets</b-nav-text>
+        </b-nav-item>
+        <b-nav-item
+          @click="onResults"
+          :active="isActive(RESULTS_ROUTE)"
+          :disabled="!hasView(RESULTS_ROUTE)"
+        >
+          <i class="fa fa-line-chart nav-icon"></i>
+          <b-nav-text>View Models</b-nav-text>
+        </b-nav-item>
+        <b-nav-item
+          @click="onPredictions"
+          :active="isActive(PREDICTION_ROUTE)"
+          :disabled="!hasView(PREDICTION_ROUTE)"
+        >
+          <i class="fa fa-eye nav-icon"></i>
+          <b-nav-text>View Predictions</b-nav-text>
+        </b-nav-item>
+    -->
+
+    <!-- Right side -->
     <b-navbar-nav class="ml-auto">
-      <b-nav-item href="/help">
-        <b-nav-text>
-          Help
-        </b-nav-text>
-      </b-nav-item>
+      <b-nav-item href="/help">Help</b-nav-item>
     </b-navbar-nav>
   </b-navbar>
 </template>
@@ -90,7 +101,7 @@
 <script lang="ts">
 import "../assets/images/uncharted.svg";
 import {
-  gotoHome,
+  // gotoHome,
   gotoSearch,
   gotoJoinDatasets,
   gotoSelectTarget,
@@ -104,7 +115,7 @@ import {
 } from "../store/app/module";
 import { getters as routeGetters } from "../store/route/module";
 import {
-  HOME_ROUTE,
+  // HOME_ROUTE,
   SEARCH_ROUTE,
   JOIN_DATASETS_ROUTE,
   SELECT_TARGET_ROUTE,
@@ -120,7 +131,7 @@ export default Vue.extend({
 
   data() {
     return {
-      HOME_ROUTE: HOME_ROUTE,
+      // HOME_ROUTE: HOME_ROUTE,
       SEARCH_ROUTE: SEARCH_ROUTE,
       JOIN_DATASETS_ROUTE: JOIN_DATASETS_ROUTE,
       SELECT_TARGET_ROUTE: SELECT_TARGET_ROUTE,
@@ -162,9 +173,9 @@ export default Vue.extend({
     isActive(view) {
       return view === this.path;
     },
-    onHome() {
-      gotoHome(this.$router);
-    },
+    // onHome() {
+    //   gotoHome(this.$router);
+    // },
     onSearch() {
       gotoSearch(this.$router);
     },
@@ -195,98 +206,71 @@ export default Vue.extend({
 });
 </script>
 
-<style>
+<style scoped>
 .navbar {
-  background-color: #424242;
-}
-.navbar-fixed-top {
-  position: fixed !important;
-  right: 0;
-  left: 0;
-}
-.nav-arrow {
-  color: rgba(255, 255, 255, 1);
-  padding-right: 5px;
-}
-.nav-icon {
-  padding: 7px;
-  width: 30px;
-  height: 30px;
-  text-align: center;
-  border-radius: 50%;
-}
-.nav-item {
-  white-space: nowrap;
-  overflow: hidden;
-}
-.nav-item .nav-link {
-  padding: 2px;
-}
-.nav-item .navbar-text {
-  letter-spacing: 0.01rem;
-}
-.navbar-nav .btn {
-  letter-spacing: 0.01rem;
-  font-weight: bold;
-}
-.navbar-nav li a .nav-icon {
-  color: white;
-  background-color: #616161;
-}
-.navbar-nav li.active a .nav-icon {
-  background-color: #1b1b1b;
-}
-.navbar-nav li.active a .navbar-text {
-  color: rgba(255, 255, 255, 1);
-}
-.navbar-nav li:hover a .nav-icon {
-  transition: 0.5s all ease;
-  color: white;
-  background-color: #1b1b1b;
-}
-.navbar-nav li:hover a .navbar-text {
-  transition: 0.5s all ease;
-  color: rgba(255, 255, 255, 1);
-}
-.navbar-nav li.active ~ li a .nav-icon {
-  color: hsla(0, 0%, 100%, 0.5);
-  background-color: inherit;
-}
-.navbar-nav li.active ~ li a .navbar-text {
-  background-color: inherit;
-}
-.navbar-nav li.active ~ li a:hover .nav-icon {
-  transition: 0.5s all ease;
-  color: white;
-  background-color: #1b1b1b;
-}
-.navbar-nav li.active ~ li a:hover .navbar-text {
-  transition: 0.5s all ease;
-  color: rgba(255, 255, 255, 1);
-}
-.session-not-ready {
-  color: #cf3835 !important;
-}
-.session-ready {
-  color: #00c07f !important;
-}
-.app-icon {
-  height: 36px;
-  margin-right: 5px;
-}
-.app-icon path {
-  fill: #c90;
-}
-.session-label {
-  padding-right: 4px;
-}
-.bottom-shadowed {
+  background-color: var(--gray-900);
   box-shadow: 0 6px 12px 0 rgba(0, 0, 0, 0.1);
 }
 
-@media (max-width: 576px) {
-  .nav-item .nav-link {
-    padding: 5px;
-  }
+.app-icon {
+  margin-right: 0.33em;
+}
+
+.nav-item {
+  font-weight: bold;
+  letter-spacing: 0.01rem;
+  overflow: hidden;
+  white-space: nowrap;
+}
+
+/* Display an arrow if two link are next to each others. */
+.nav-item + .nav-item .nav-link::before {
+  color: var(--gray-600);
+  content: "\f105"; /* angle-right => https://fontawesome.com/v4.7.0/cheatsheet/ */
+  font-family: FontAwesome;
+  font-weight: bold;
+  margin-right: 1em;
+}
+
+/* Icon. */
+.nav-icon {
+  border-radius: 50%;
+  height: 30px;
+  margin-right: 0.25em;
+  padding: 7px;
+  text-align: center;
+  width: 30px;
+}
+
+/* 
+  In the following I use the ID #distil-app to overwrite the Bootstrap CSS
+  by increasing the selectors specificity. 
+*/
+
+/* Default colours */
+#distil-app .nav-link {
+  transition: color 0.25s;
+  color: var(--gray-600);
+}
+#distil-app .nav-link .nav-icon {
+  transition: background 0.25s, color 0.25s;
+  background-color: var(--gray-800);
+  color: var(--gray-400);
+}
+
+/* Active and non disabled on hover nav-item */
+#distil-app .nav-link.active,
+#distil-app .nav-link:not(.disable):hover {
+  color: var(--gray-400);
+}
+#distil-app .nav-link.active .nav-icon,
+#distil-app .nav-link:not(.disable):hover .nav-icon {
+  background-color: var(--black);
+}
+
+/* Disabled Nav-item */
+#distil-app .nav-link.disabled .nav-icon {
+  background: none;
+  color: var(--gray-600);
 }
 </style>
