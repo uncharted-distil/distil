1d50aea7cd5d0113cb8a304a476f05be63f79463
Yohann Paris <yparis@uncharted.software>
2020-09-11 16:18:36 -0400

    Explain to the user that if their dataset is bigger than `INGEST_SAMPLE_ROW_LIMIT` we sampled it. I added a button to let the user reimport the dataset without sampling. This was done by making the sampling limit the Max value of Go MaxInt32, so this might be a future bottle neck if we have more two billions rows (2,147,483,647).

--- public/components/FileUploaderStatus.vue
+++ public/components/FileUploaderStatus.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
+    <!-- Waiting -->
     <b-alert :show="status === 'started'" variant="info">
       Importing <b>{{ filename }}</b> as <b>{{ datasetID }}</b
       >...
     </b-alert>
+
+    <!-- Success -->
     <b-alert :show="status === 'success'" dismissible variant="success">
       <i class="fa fa-check-circle-o" aria-hidden="true"></i> Imported
       <b>{{ filename }}</b> as <b>{{ datasetID }}</b>
+      <template v-if="isSampling">
+        &mdash; Because of its size, the dataset has been sampled to
+        {{ rowCount }} rows.
+        <b-button @click="onClick" variant="success" size="sm">
+          <i class="fa fa-download" aria-hidden="true"></i>
+          Import the full dataset
+        </b-button>
+      </template>
     </b-alert>
+
+    <!-- Error -->
     <b-alert :show="status === 'error'" dismissible variant="danger">
       <i class="fa fa-times-circle-o" aria-hidden="true"></i> An unexpected
       error has happened while importing <b>{{ filename }}</b>
@@ -20,19 +33,32 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "file-uploader-status",
+
   props: {
+    datasetID: String,
+    filename: String,
+    importResponse: Object,
+    numRows: Number,
     status: {
       type: String,
       required: true,
     },
-    filename: {
-      type: String,
+  },
+
+  computed: {
+    isSampling(): boolean {
+      return !!this.importResponse?.sampled;
     },
-    datasetID: {
-      type: String,
+
+    rowCount(): number {
+      return this.importResponse?.rowCount ?? 0;
+    },
+  },
+
+  methods: {
+    onClick() {
+      this.$emit("importfull");
     },
   },
 });
 </script>
-
-<style></style>
