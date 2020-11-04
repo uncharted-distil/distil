dee6e9da3858ab24ac862ef52ee5bf1e8337854f
philippe horne <phorne@uncharted.software>
2020-11-04 14:57:49 -0500

    Added temporary extract route that extracts a dataset from storage and writes it to disk.

---
+++ api/routes/extract.go
@@ -0,0 +1,63 @@
+//
+//   Copyright © 2019 Uncharted Software Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package routes
+
+import (
+	"net/http"
+
+	"github.com/pkg/errors"
+	"goji.io/v3/pat"
+
+	"github.com/uncharted-distil/distil/api/env"
+	api "github.com/uncharted-distil/distil/api/model"
+	"github.com/uncharted-distil/distil/api/task"
+)
+
+// ExtractHandler extracts a dataset from storage and writes it to disk.
+func ExtractHandler(metaCtor api.MetadataStorageCtor, dataCtor api.DataStorageCtor, config env.Config) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// get dataset name
+		dataset := pat.Param(r, "dataset")
+
+		// get storage clients
+		metaStorage, err := metaCtor()
+		if err != nil {
+			handleError(w, err)
+			return
+		}
+		dataStorage, err := dataCtor()
+		if err != nil {
+			handleError(w, err)
+			return
+		}
+
+		datasetName, datasetPath, err := task.ExportDataset(dataset, metaStorage, dataStorage)
+		if err != nil {
+			handleError(w, err)
+			return
+		}
+
+		// marshal output into JSON
+		err = handleJSON(w, map[string]interface{}{
+			"dataset":     datasetName,
+			"datasetPath": datasetPath,
+		})
+		if err != nil {
+			handleError(w, errors.Wrap(err, "unable marshal clustering result into JSON"))
+			return
+		}
+	}
+}
