279c06a4b6631bbef5faa0bf68e9179c6591125d
Yohann Paris <yparis@uncharted.software>
2020-12-07 17:13:10 -0500

    New ptional parameter to run TA3 with the Docker containers on another host.

--- run.sh
+++ run.sh
@@ -12,8 +12,16 @@ if [[ -z "${DATAMART_IMPORT_FOLDER}" ]]; then
   export DATAMART_IMPORT_FOLDER="$PWD/datamart"
 fi
 
-export SOLUTION_COMPUTE_ENDPOINT=localhost:45042
-export ES_ENDPOINT=http://localhost:9200
+# Docker host IP for the services started by ./run_services.sh
+if [[ -z ${DOCKER_HOST} ]]; then 
+  export SOLUTION_COMPUTE_ENDPOINT=localhost:45042
+  export ES_ENDPOINT=http://localhost:9200
+else 
+  export SOLUTION_COMPUTE_ENDPOINT="${DOCKER_HOST}:45042"
+  export ES_ENDPOINT="http://${DOCKER_HOST}:9200"
+  export PG_HOST="${DOCKER_HOST}"
+fi
+
 export SOLUTION_COMPUTE_TRACE=true
 export PG_LOG_LEVEL=none # debug, error, warn, info, none
 export SOLUTION_SEARCH_MAX_TIME=30000
@@ -22,7 +30,7 @@ export SOLUTION_COMPUTE_PULL_TIMEOUT=60000
 export DATAMART_URL_NYU=https://auctus.vida-nyu.org
 export CLUSTERING_ENABLED=false # no image clustering on ingest
 export SUMMARY_ENABLED=false # no duke summarization on ingest
-export FEATURIZATION_ENABLED=true # featurize rs imagery on ingest
+export FEATURIZATION_ENABLED=true # featurize remote sensing imagery on ingest
 export CLUSTERING_KMEANS=true # 'true' if kmeans should be used for clustering 'false' if we should use hdbscan
 export TILE_REQUEST_URL=https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png
 # export MAX_TRAINING_ROWS=500
