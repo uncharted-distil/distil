8d0bed182f3cf78a11b5fd18af1a2330d9eaa53a
philippe horne <phorne@uncharted.software>
2021-02-01 08:31:51 -0500

    Added prefeaturized dataset metadata filtering.

--- api/compute/filter.go
+++ api/compute/filter.go
@@ -55,33 +55,8 @@ func filterData(client *compute.Client, ds *api.Dataset, filterParams *api.Filte
 		return outputFolder, updatedParams, nil
 	}
 
-	// read the data from the database
-	data, err := dataStorage.FetchDataset(ds.ID, ds.StorageName, true, false, nil)
-	if err != nil {
-		return "", nil, err
-	}
-
-	// update the metadata to match the data pulled from the data storage
-	// (mostly matching column index and dropping columns not pulled)
-	metaVarMap := map[string]*model.Variable{}
-	for _, v := range ds.Variables {
-		metaVarMap[v.Key] = v
-	}
-	variablesData := make([]*model.Variable, len(data[0]))
-	for i, f := range data[0] {
-		variablesData[i] = metaVarMap[f]
-		variablesData[i].Index = i
-	}
-	ds.Variables = variablesData
-
-	// write it out as a dataset
-	dsRaw := &api.RawDataset{
-		ID:       ds.ID,
-		Name:     ds.Name,
-		Data:     data,
-		Metadata: ds.ToMetadata(),
-	}
-	err = serialization.WriteDataset(outputFolder, dsRaw)
+	// prepare the data to use for filtering
+	outputFolder, err = preparePrefilteringDataset(outputFolder, ds, dataStorage)
 	if err != nil {
 		return "", nil, err
 	}
@@ -151,4 +126,124 @@ func getPreFiltering(ds *api.Dataset, filterParams *api.FilterParams) (*api.Filt
 	}
 
 	return clone, preFilters
+}
+
+func preparePrefilteringDataset(outputFolder string, sourceDataset *api.Dataset, dataStorage api.DataStorage) (string, error) {
+	// read the data from the database
+	data, err := dataStorage.FetchDataset(sourceDataset.ID, sourceDataset.StorageName, true, false, nil)
+	if err != nil {
+		return "", err
+	}
+
+	// if learning dataset, then update that
+	if sourceDataset.LearningDataset != "" {
+		return UpdatePrefeaturizedDataset(sourceDataset.LearningDataset, sourceDataset, data)
+	}
+
+	// update the metadata to match the data pulled from the data storage
+	// (mostly matching column index and dropping columns not pulled)
+	metaVarMap := map[string]*model.Variable{}
+	for _, v := range sourceDataset.Variables {
+		metaVarMap[v.Key] = v
+	}
+	variablesData := make([]*model.Variable, len(data[0]))
+	for i, f := range data[0] {
+		variablesData[i] = metaVarMap[f]
+		variablesData[i].Index = i
+	}
+	sourceDataset.Variables = variablesData
+
+	// write it out as a dataset
+	dsRaw := &api.RawDataset{
+		ID:       sourceDataset.ID,
+		Name:     sourceDataset.Name,
+		Data:     data,
+		Metadata: sourceDataset.ToMetadata(),
+	}
+	err = serialization.WriteDataset(outputFolder, dsRaw)
+	if err != nil {
+		return "", err
+	}
+
+	return outputFolder, nil
+}
+
+// UpdatePrefeaturizedDataset updates a featurized dataset that already exists
+// on disk to have new variables included
+func UpdatePrefeaturizedDataset(prefeaturizedPath string, sourceDataset *api.Dataset, storedData [][]string) (string, error) {
+	// load the dataset from disk
+	schemaPath := path.Join(prefeaturizedPath, compute.D3MDataSchema)
+	dsDisk, err := serialization.ReadDataset(schemaPath)
+	if err != nil {
+		return "", err
+	}
+	metaDiskMainDR := dsDisk.Metadata.GetMainDataResource()
+
+	// determine if there are new columns that were not part of the original dataset
+	metaDiskVarMap := MapVariables(metaDiskMainDR.Variables, func(variable *model.Variable) string { return variable.Key })
+	newVars := []*model.Variable{}
+	for _, v := range sourceDataset.Variables {
+		if metaDiskVarMap[v.Key] == nil {
+			newVars = append(newVars, v)
+		}
+	}
+
+	// get the index of the new fields in the extracted data
+	storedVarMap := MapVariables(sourceDataset.Variables, func(variable *model.Variable) string { return variable.Key })
+	storedDataD3MIndex := -1
+	for i, v := range storedData[0] {
+		if v == model.D3MIndexFieldName {
+			storedDataD3MIndex = i
+		} else if storedVarMap[v] != nil {
+			storedVarMap[v].Index = i
+		}
+	}
+
+	// add the missing columns row by row and only retain rows in the new dataset
+	// first build up the new variables by d3m index map
+	// then cycle through the featurized rows and append the variables
+	newDataMap := map[string][]string{}
+	for _, r := range storedData[1:] {
+		newVarsData := []string{}
+		for i := 0; i < len(newVars); i++ {
+			newVarsData = append(newVarsData, r[newVars[i].Index])
+		}
+		newDataMap[r[storedDataD3MIndex]] = newVarsData
+	}
+
+	// add the new fields to the metadata to generate the proper header
+	for i := 0; i < len(newVars); i++ {
+		newVar := newVars[i]
+		newVar.Index = i + len(metaDiskMainDR.Variables)
+		metaDiskMainDR.Variables = append(metaDiskMainDR.Variables, newVar)
+	}
+
+	preFeaturizedOutput := [][]string{metaDiskMainDR.GenerateHeader()}
+	metaDiskD3MIndex := metaDiskVarMap[model.D3MIndexFieldName].Index
+	for _, row := range dsDisk.Data[1:] {
+		d3mIndexPre := row[metaDiskD3MIndex]
+		if newDataMap[d3mIndexPre] != nil {
+			rowComplete := append(row, newDataMap[d3mIndexPre]...)
+			preFeaturizedOutput = append(preFeaturizedOutput, rowComplete)
+		}
+	}
+
+	// output the new pre featurized data
+	dsDisk.Data = preFeaturizedOutput
+	err = serialization.WriteDataset(prefeaturizedPath, dsDisk)
+	if err != nil {
+		return "", err
+	}
+
+	return prefeaturizedPath, nil
+}
+
+// MapVariables creates a variable map using the mapper function to create the key.
+func MapVariables(variables []*model.Variable, mapper func(variable *model.Variable) string) map[string]*model.Variable {
+	mapped := map[string]*model.Variable{}
+	for _, d := range variables {
+		mapped[mapper(d)] = d
+	}
+
+	return mapped
 }
