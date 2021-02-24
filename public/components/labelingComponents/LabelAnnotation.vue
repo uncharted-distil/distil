c2c4e44289751693346f4f977a54086b2e6ca395
zac <codinfreakgf@gmail.com>
2021-02-24 16:24:02 -0500

    updated icon and label view divider lines
    divider lines are now the same as the result view

--- public/components/labelingComponents/LabelAnnotation.vue
+++ public/components/labelingComponents/LabelAnnotation.vue
@@ -1,5 +1,8 @@
 <template>
-  <i :class="getLabel" aria-hidden="true"></i>
+  <span class="stacked-icons">
+    <i v-if="shouldRender" class="fa fa-circle fa-stack-1x p-1" />
+    <i :class="getLabel" />
+  </span>
 </template>
 
 <script lang="ts">
@@ -14,15 +17,18 @@ export default Vue.extend({
     getLabel(): string {
       switch (this.item[LOW_SHOT_LABEL_COLUMN_NAME].value) {
         case LowShotLabels.positive:
-          return "fa fa-check text-success p-1";
+          return "fa fa-plus-circle text-success p-1 fa-stack-1x";
           break;
         case LowShotLabels.negative:
-          return "fa fa-times red p-1";
+          return "fa fa-minus-circle red p-1 fa-stack-1x";
           break;
         default:
           return "d-none";
           break;
       }
+    },
+    shouldRender(): boolean {
+      return this.getLabel !== "d-none";
     },
   },
 });
@@ -31,5 +37,13 @@ export default Vue.extend({
 <style scoped>
 .red {
   color: var(--red);
+}
+.stacked-icons {
+  position: relative;
+  display: inline-block;
+  width: 1em;
+  height: 1em;
+  line-height: 1em;
+  vertical-align: middle;
 }
 </style>
