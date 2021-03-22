4395e086774ad68038612b296bd1aa6dc6b19d97
Chris Bethune <cbethune@uncharted.software>
2021-03-22 14:03:24 -0400

    filter features that cannot be ranked using mutual information

--- api/task/target_rank.go
+++ api/task/target_rank.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/uncharted-distil/distil-compute/metadata"
@@ -29,7 +30,7 @@ import (
 	log "github.com/unchartedsoftware/plog"
 )
 
-var exclusions = map[string]bool{
+var excludedTypes = map[string]bool{
 	model.MultiBandImageType: true,
 	model.ImageType:          true,
 	model.TimeSeriesType:     true,
@@ -38,27 +39,35 @@ var exclusions = map[string]bool{
 	model.GeoCoordinateType:  true,
 }
 
+var excludedRoles = map[string]bool{
+	model.VarDistilRoleMetadata:   true,
+	model.VarDistilRoleSystemData: true,
+}
+
 // TargetRank will rank the dataset relative to a target variable using
 // a primitive.
 func TargetRank(dataset string, target string, features []*model.Variable, source metadata.DatasetSource) (map[string]float64, error) {
-	// Some feature types cannot be / should not be ranked.  We remove them from the feature list, and completely skip ranking if there
-	// isn't at least 3 valid (target + 2 features)
-	filteredFeatures := []*model.Variable{}
+	// Some feature types cannot be / should not be ranked - we should skip ranking if there
+	// aren't at least 3 valid (target + 2 features)
+
+	// find target feature
 	var targetFeature *model.Variable
 	for _, feature := range features {
-		if feature.Key == target {
+		if strings.EqualFold(feature.Key, target) {
 			targetFeature = feature
+			break
 		}
-		if !exclusions[feature.Type] {
-			filteredFeatures = append(filteredFeatures, feature)
-		}
-	}
-	if len(filteredFeatures) <= 2 || exclusions[targetFeature.Type] {
-		return map[string]float64{}, nil
 	}
 
-	// create & submit the solution request
-	pip, err := description.CreateTargetRankingPipeline("target_rank", "feature ranking relative to the target", target, filteredFeatures)
+	// filter features by type / role and skip if we don't have at least 2 features that are valid
+	filteredFeatures := filterFeatures(features, target)
+	if len(filteredFeatures) <= 2 || excludedTypes[targetFeature.Type] {
+		return map[string]float64{}, nil
+	}
+	filteredFeatures[target] = true
+
+	// create & submit the solution request - we send the list of filtered features to ensure that
+	pip, err := description.CreateTargetRankingPipeline("target_rank", "feature ranking relative to the target", targetFeature, features, filteredFeatures)
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to create ranking pipeline")
 	}
@@ -99,4 +108,35 @@ func TargetRank(dataset string, target string, features []*model.Variable, sourc
 	}
 
 	return ranks, nil
+}
+
+func filterFeatures(features []*model.Variable, target string) map[string]bool {
+	filteredFeatures := map[string]*model.Variable{}
+
+	groupedVars := []*model.Variable{}
+	for _, feature := range features {
+		// if this is a grouped var save for follow on processing
+		if feature.IsGrouping() {
+			groupedVars = append(groupedVars, feature)
+		}
+
+		// check if this is a feature that we've marked as elligible for ranking and save it if so
+		if !excludedTypes[feature.Type] && !excludedRoles[feature.DistilRole] && feature.Key != target {
+			filteredFeatures[feature.Key] = feature
+		}
+	}
+
+	// remove any variables that are rankable, but are marked as hidden by their parent grouping
+	for _, groupedVar := range groupedVars {
+		hidden := groupedVar.Grouping.GetHidden()
+		for _, hiddenVar := range hidden {
+			delete(filteredFeatures, hiddenVar)
+		}
+	}
+
+	featureList := map[string]bool{}
+	for _, feature := range filteredFeatures {
+		featureList[strings.ToLower(feature.Key)] = true
+	}
+	return featureList
 }
