b7c5c4a4eedc164387ee3ea111134888e43243a1
philippe horne <phorne@uncharted.software>
2021-05-07 15:51:19 -0400

    Fixed return type of delete and clear actions.

--- public/store/dataset/actions.ts
+++ public/store/dataset/actions.ts
@@ -692,7 +692,7 @@ export const actions = {
       highlights: Highlight[];
       filterParams: FilterParams;
     }
-  ): Promise<[void[], void[]]> {
+  ): Promise<[void, void]> {
     if (!validateArgs(args, ["dataset", "key"])) {
       return null;
     }
@@ -735,7 +735,7 @@ export const actions = {
   async deleteVariable(
     context: DatasetContext,
     args: { dataset: string; key: string }
-  ): Promise<[void[], void[]]> {
+  ): Promise<[void, void]> {
     if (!validateArgs(args, ["dataset", "key"])) {
       return null;
     }
