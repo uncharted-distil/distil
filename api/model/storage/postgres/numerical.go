a9dd572e9149cc6d45a0411746d32499004ffbf2
zac <codinfreakgf@gmail.com>
2021-06-03 13:10:53 -0400

    added updateClusterHighlight check to summary fetch

--- api/model/storage/postgres/numerical.go
+++ api/model/storage/postgres/numerical.go
@@ -187,7 +187,9 @@ func (f *NumericalField) fetchHistogramWithJoins(filterParams *api.FilterParams,
 
 func (f *NumericalField) fetchHistogramByResult(resultURI string, filterParams *api.FilterParams, extrema *api.Extrema, numBuckets int) (*api.Histogram, error) {
 	fromClause := f.getFromClause(false)
-
+	if err := f.updateClusterHighlight(filterParams, api.DefaultMode); err != nil {
+		return nil, err
+	}
 	// get filter where / params
 	wheres, params, err := f.Storage.buildResultQueryFilters(f.GetDatasetName(), f.DatasetStorageName, resultURI, filterParams, baseTableAlias)
 	if err != nil {
